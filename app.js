f531414fe02214ff0eb1ab3a41881b3f1bf23c76
Hriday Keswani <hridaykeswani@gmail.com>
2023-04-02 00:35:58 +0530

    implement api changes

--- app.js
+++ app.js
@@ -6,10 +6,14 @@ app.use(bodyParser.json())
 
 var port = 9000;
 
+app.get('/', function(req,res){
+    res.send('works');
+});
+
 app.post('/sample', function(req, res) {
     console.log('receiving data ...');
     res.send(req.body);
-    console.log(`Receiveed:\n ${req.body}`)
+    console.log(`Receiveed:\nlat:${req.body.latitude}\nlong:${req.body.longitude}`)
 });
 
 app.listen(port);
